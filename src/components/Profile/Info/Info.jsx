e20476c712b3acdc4fd544ef53a96506e2549e36
Rostislav <spellmane21@gmail.com>
2021-12-20 11:11:57 +0200

    85 hooks

--- src/components/Profile/Info/Info.jsx
+++ src/components/Profile/Info/Info.jsx
@@ -2,29 +2,29 @@ import s from './Info.module.css';
 import React from 'react';
 import Preloader from '../../common/Preloader/Preloader'
 import zoro from '../../../assets/images/zoro.jpg'
-import ProfileStatus from './ProfileStatus'
+import {ProfileStatusHook} from './ProfileStatusHook'
 function Info(props) {
     console.log(props)
     if (!props.profile) {
         return <Preloader />
-        
+
     }
-    
+
     return (
         <div className={s.row}>
-            <div><img src={props.profile.photos.large ? props.profile.photos.large : zoro} alt="logo" className={s.logo}/></div>
+            <div><img src={props.profile.photos.large ? props.profile.photos.large : zoro} alt="logo" className={s.logo} /></div>
             <div className={s.info}>
                 <div className={s.name}>
                     {props.profile.fullName}
                 </div>
                 <ul className={s.column}>
-                
+
                     <li className={s.point}>Date of Birth: 2 January</li>
                     <li className={s.point}>City: Minsk</li>
                     <li className={s.point}>Education: BSU'11</li>
                     <li className={s.point}>Web Site: {props.profile.contacts.twitter}</li>
                 </ul>
-                <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+                <ProfileStatusHook status={props.status} updateStatus={props.updateStatus} />
             </div>
         </div>
     )
---
+++ src/components/Profile/Info/ProfileStatusHook.jsx
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from "react";
+import s from './Info.module.css';
+export const ProfileStatusHook = (props) => {
+    let [editMode, setEditMode] = useState(false)
+    let [status, setStatus] = useState(props.status)
+
+    useEffect( () => {
+        setStatus(props.status)
+    }, [props.status])
+
+    let activateEditMode = () => {
+        setEditMode(true)
+    }
+    let deactivateEditMode = () => {
+        setEditMode(false)
+        props.updateStatus(status);
+    }
+    let onStatusChange = (e) => {
+        setStatus(e.currentTarget.value)
+    }
+        return (
+            <div>
+                { !editMode &&
+                    <div className={s.point} onDoubleClick={activateEditMode}>
+                        Status: {props.status || 'There`s no status'}
+                    </div>
+                }
+                { editMode &&
+                    <input type="text" className={s.point} autoFocus={true} onBlur={deactivateEditMode} onChange={onStatusChange} value={status}/>
+                }
+            </div>
+        )
+}
